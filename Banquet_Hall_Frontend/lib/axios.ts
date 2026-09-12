59f0160d1f7d640df3db2e9937306c59b97359f5
Deep Sarkar <161959401+deep-23sarkar@users.noreply.github.com>
2026-09-13 04:42:30 +0530

    Axios changes

--- Banquet_Hall_Frontend/lib/axios.ts
+++ Banquet_Hall_Frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL : `${process.env.NEXT_PUBLIC_SPRING_BACKEND_API_URL}/admin`,
+    baseURL : `${process.env.NEXT_PUBLIC_SPRING_BACKEND_API_URL}`,
     headers :{
         "Content-Type" : "application/json"
     }
